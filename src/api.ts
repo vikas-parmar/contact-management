852ac1676827fac2cf9a2f336d8eabb0ca8fa972
Vikas Parmar <vkparmar003@gmail.com>
2023-08-26 22:01:35 +0530

    query setup

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+
+const API_BASE = 'https://disease.sh/v3/covid-19';
+
+export const useWorldData = () => {
+    return useQuery('worldData', async () => {
+        const response = await fetch(`${API_BASE}/all`);
+        const data = await response.json();
+        return data;
+    });
+};
+
+export const useCountryData = () => {
+    return useQuery('countryData', async () => {
+        const response = await fetch(`${API_BASE}/countries`);
+        const data = await response.json();
+        return data;
+    });
+};
