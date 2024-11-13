ad406f0162c1a393be0ab07099bc60e88fead665
Vikas Parmar <vkparmar003@gmail.com>
2024-11-13 16:36:51 +0530

    setup routes and components

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,53 @@
+import { Key, useEffect } from "react";
 import {
   Form,
   Links,
+  LiveReload,
   Meta,
+  NavLink,
+  Outlet,
+  redirect,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
+import {
+  LinksFunction,
+  LoaderFunctionArgs,
+  MetaFunction,
+} from "@remix-run/node";
+import { createEmptyContact, getContacts } from "./data";
+import appStylesHref from "./app.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+// provides data to the component
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return Response.json({ contacts, q });
+};
 
 export default function App() {
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
   return (
     <html lang="en">
       <head>
@@ -19,35 +60,104 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              role="search"
+            >
               <input
-                id="q"
                 aria-label="Search contacts"
+                className={searching ? "loading" : ""}
+                defaultValue={q || ""}
+                id="q"
+                name="q"
                 placeholder="Search"
                 type="search"
-                name="q"
+                onChange={(event) => submit(event.currentTarget)}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div aria-hidden hidden={!searching} id="search-spinner" />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map(
+                  (contact: {
+                    id: Key | null | undefined;
+                    first: string;
+                    last: string;
+                    favorite: string;
+                  }) => (
+                    <li key={contact.id}>
+                      <NavLink
+                        className={({ isActive, isPending }) =>
+                          isActive ? "active" : isPending ? "pending" : ""
+                        }
+                        to={`contacts/${contact.id}`}
+                      >
+                        {contact.first || contact.last ? (
+                          <>
+                            {contact.first} {contact.last}
+                          </>
+                        ) : (
+                          <i>No Name</i>
+                        )}{" "}
+                        {contact.favorite ? <span>★</span> : null}
+                      </NavLink>
+                    </li>
+                  )
+                )}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+
+        <div
+          id="detail"
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
+        >
+          <Outlet />
         </div>
 
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
 }
+
+// updates persistent data
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+// Meta descriptions
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Contacts" },
+    {
+      property: "og:title",
+      content: "Contact App using Remix",
+    },
+    {
+      name: "description",
+      content: "Contact Management App. Build with Remix framework.",
+    },
+  ];
+};
